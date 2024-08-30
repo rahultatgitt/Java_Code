0fea8a4410ed9ec7d842ae78cb29c9f69b5829a2
rahulatgitt <rahulsingh11158@gmail.com>
2024-08-30 23:28:21 +0530

    Fibonacci series

---
+++ fibonacci.java
@@ -0,0 +1,17 @@
+public class fibonacci {
+    public static void main(String[] args) {
+        int n = 20; // Number of terms to display
+        int firstTerm = 0, secondTerm = 1;
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + " ");
+
+            // Compute the next term
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
