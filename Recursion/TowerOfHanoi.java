0050e6efe0941876d76da934746fbcb210230406
rahulatgitt <rahulsingh11158@gmail.com>
2024-09-03 22:32:40 +0530

    Tower of Hanoi added

---
+++ Recursion/TowerOfHanoi.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+public class TowerOfHanoi {
+
+    public static void solveHanoi(int n, char fromRod, char toRod, char auxRod) {
+        if (n == 1) {  // Base case: only one disk to move
+            System.out.println("Move disk 1 from rod " + fromRod + " to rod " + toRod);
+            return;
+        }
+        
+        // Move n-1 disks from the source rod to the auxiliary rod
+        solveHanoi(n - 1, fromRod, auxRod, toRod);
+        
+        // Move the nth disk from the source rod to the destination rod
+        System.out.println("Move disk " + n + " from rod " + fromRod + " to rod " + toRod);
+        
+        // Move the n-1 disks from the auxiliary rod to the destination rod
+        solveHanoi(n - 1, auxRod, toRod, fromRod);
+    }
+
+    public static void main(String[] args) {
+        int numDisks = 3;
+        solveHanoi(numDisks, 'A', 'C', 'B');
+    }
+
+    
+}
